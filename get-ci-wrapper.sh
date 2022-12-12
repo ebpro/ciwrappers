5c82d716693951c104a908a5eea07c494d57be7c
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2022-12-12 16:56:01 +0100

    adds an install script.

---
+++ get-ci-wrapper.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+_create_config() (
+  # Create or update default config file
+  mkdir -p "${HOME}/.ci-wrappers/"
+  cat >!"${HOME}/.ci-wrappers/config" <<END
+CI_VAGRANT_VERSION=2.3.4
+CI_TERRAFORM_VERSION=1.3.6
+CI_DOCKER_CLIENT_VERSION=20.10.19
+CI_DOCKER_COMPOSE_VERSION=2.13.0
+CI_GH_CLI_VERSION=2.20.2
+
+CI_MAVEN_DEFAULT_IMAGE="\${CI_MAVEN_DEFAULT_IMAGE:-brunoe/maven:3.8.6-eclipse-temurin-17}"
+CI_JAVA_DEFAULT_ARCHETYPE_GROUPID="\${CI_JAVA_DEFAULT_ARCHETYPE_GROUPID:-fr.univtln.bruno.demos.archetypes}"
+CI_JAVA_DEFAULT_ARCHETYPE_ARTIFACTID="\${CI_JAVA_DEFAULT_ARCHETYPE_ARTIFACTID:-demomavenarchetype}"
+CI_JAVA_DEFAULT_ARCHETYPE_VERSION="\${CI_JAVA_DEFAULT_ARCHETYPE_VERSION:-1.1-SNAPSHOT}"
+
+export CI_WRAPPERS_HOME="\${CI_WRAPPERS_HOME:-\${HOME}/.ci-wrappers}"
+export DOCKER_CONFIG="\$CI_WRAPPERS_HOME/.docker"
+END
+)
+
+# Creates a default config files.
+_create_config
+
+# Installs the script locally
+curl -s https://raw.githubusercontent.com/ebpro/ciwrappers/develop/ci-wrappers.sh > "$CI_WRAPPERS_HOME"/ci-wrappers.sh
